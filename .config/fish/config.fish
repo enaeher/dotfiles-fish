b2d8fc1c550d6ae56be431c028ee822937e64b7a
Eli Naeher <eli@outpace.com>
2014-09-15 18:54:15 -0500

    Add some Outpace stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,6 @@ set -x BROWSER chromium
 set -x EDITOR emacsclient
 set -x ALTERNATE_EDITOR "emacs --daemon"
 set -x GOPATH $HOME/go
-set -x PATH $PATH $GOPATH/bin $HOME/.gem/ruby/2.1.0/bin $HOME/.gem/ruby/2.0.0/bin 
+set -x STARWOOD_HOME $HOME/work/starwood_guest/universe
+set -x PATH $PATH $GOPATH/bin $HOME/.gem/ruby/2.1.0/bin $HOME/.gem/ruby/2.0.0/bin $STARWOOD_HOME/bin
 set fish_greeting ""
