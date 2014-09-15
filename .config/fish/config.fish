a9764916d070f91df80aa88f243f633ccc56a6c6
Eli Naeher <eli@outpace.com>
2014-09-14 21:12:55 -0500

    Remove old paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,5 @@ set -x BROWSER chromium
 set -x EDITOR emacsclient
 set -x ALTERNATE_EDITOR "emacs --daemon"
 set -x GOPATH $HOME/go
-set -x CLASSPATH $HOME/git/gcsb/server/bin
-set -x PATH $PATH $GOPATH/bin $HOME/.gem/ruby/2.1.0/bin $HOME/.gem/ruby/2.0.0/bin $HOME/git/gcsb/server/bin 
+set -x PATH $PATH $GOPATH/bin $HOME/.gem/ruby/2.1.0/bin $HOME/.gem/ruby/2.0.0/bin 
 set fish_greeting ""
