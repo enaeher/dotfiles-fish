9e10ec6bebd6c50c211d8a1facc7ec7a1b89f076
Eli Naeher <eli@outpace.com>
2014-09-17 14:10:12 -0500

    NGINX_URL required for outpace tests

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,5 @@ set -x ALTERNATE_EDITOR "emacs --daemon"
 set -x GOPATH $HOME/go
 set -x STARWOOD_HOME $HOME/work/starwood_guest/universe
 set -x PATH $PATH $GOPATH/bin $HOME/.gem/ruby/2.1.0/bin $HOME/.gem/ruby/2.0.0/bin $STARWOOD_HOME/bin
+set -x NGINX_URL http://localhost:7999
 set fish_greeting ""
