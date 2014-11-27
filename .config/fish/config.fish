8aad8d70fb0c03be2fb6395c5be430fcfb46af01
Eli Naeher <eli@outpace.com>
2014-11-26 19:07:23 -0600

    informative git prompt is now part of fish. Also source autojump file (this is currently OSX-only, should be generalized or branched)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,9 @@ set -x GOPATH $HOME/go
 set -x STARWOOD_HOME $HOME/work/starwood_guest/universe
 set -x PATH $PATH $GOPATH/bin $HOME/.gem/ruby/2.1.0/bin $HOME/.gem/ruby/2.0.0/bin $STARWOOD_HOME/bin
 set -x NGINX_URL http://localhost:7999
-set fish_greeting ""
+set -x OUTPACE_PEM /Users/eli/work/starwood_guest/universe/common_utilities/setup/outpace.pem
+source /usr/local/Cellar/autojump/21.7.1/etc/autojump.fish
+set fish_greeting "\"But fish don't have shells!\""
+function fish_title
+  true
+end
