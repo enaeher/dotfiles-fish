bad9b0d26d7c5d297cd218a800ec1a96bb070f14
Eli Naeher <eli@outpace.com>
2015-02-06 17:32:08 -0600

    Set EC2_HOME and JAVA_HOME

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,8 +6,11 @@ set -x STARWOOD_HOME $HOME/work/starwood_guest/universe
 set -x PATH $PATH $GOPATH/bin $HOME/.gem/ruby/2.1.0/bin $HOME/.gem/ruby/2.0.0/bin $STARWOOD_HOME/bin
 set -x NGINX_URL http://localhost:7999
 set -x OUTPACE_PEM /Users/eli/work/starwood_guest/universe/common_utilities/setup/outpace.pem
+set -x EC2_HOME /usr/local/Cellar/ec2-api-tools/1.7.3.0/libexec
+set -x JAVA_HOME (/usr/libexec/java_home)
+
 source /usr/local/Cellar/autojump/21.7.1/etc/autojump.fish
-set fish_greeting "\"But fish don't have shells!\""
+set fish_greeting "Welcome."
 function fish_title
   true
 end
