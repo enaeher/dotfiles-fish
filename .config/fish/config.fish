c9e50d26b04ece02d88ec871b7ed68376500460a
Eli Naeher <eli@naeher.name>
2019-01-08 16:18:17 -0500

    Bump autojump, remove chruby and gnupg, some env var cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,11 @@
 set -x BROWSER chromium
 set -x EDITOR "emacsclient -t"
 set -x ALTERNATE_EDITOR "emacs --daemon"
-set -x PATH $PATH /usr/local/sbin /Users/eli/work/healthfinch/dotfiles/bin /Users/eli/work/healthfinch/infrastructure/bin
-set -x NGINX_URL http://localhost:7999
-set -x EC2_HOME /usr/local/Cellar/ec2-api-tools/1.7.3.0/libexec
 set -x JAVA_HOME (/usr/libexec/java_home)
-set -x DISABLE_SPRING true
+set -x CLUBHOUSE_HOME /Users/elinaeher/clubhouse
+set -x AWS_PROFILE clubhouse
 
-source /usr/local/Cellar/autojump/22.2.4/share/autojump/autojump.fish
+source /usr/local/Cellar/autojump/22.5.1/share/autojump/autojump.fish
 set fish_greeting "Welcome."
 function fish_title
   true
@@ -15,6 +13,3 @@ end
 
 set -x GPG_TTY (tty)
 
-source $HOME/.config/fish/functions/gnupg.fish
-source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
