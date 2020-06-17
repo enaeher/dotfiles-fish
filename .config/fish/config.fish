1af1724a3d1258c260ffe7b8e83f7084988a5e62
Eli <elin@clubhouse.io>
2020-06-17 11:31:47 -0400

    various

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,16 @@
 set -x BROWSER chromium
 set -x EDITOR "emacsclient -t"
 set -x ALTERNATE_EDITOR "emacs --daemon"
-set -x JAVA_HOME (/usr/libexec/java_home)
-set -x CLUBHOUSE_HOME /Users/elinaeher/clubhouse
+set -x CLUBHOUSE_HOME /Users/eli/clubhouse
 set -x AWS_PROFILE clubhouse
+set -x AWS_VAULT_BACKEND keychain
+set -x AWS_VAULT_KEYCHAIN_NAME login
 
-source /usr/local/Cellar/autojump/22.5.1/share/autojump/autojump.fish
+set -x JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+set -x GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-ce-java11-19.3.1/Contents/Home
+set -x PATH $PATH $CLUBHOUSE_HOME/backend/bin $CLUBHOUSE_HOME/backend/infrastructure/build-support $GRAALVM_HOME/bin
+
+source /usr/local/Cellar/autojump/22.5.3/share/autojump/autojump.fish
 set fish_greeting "Welcome."
 function fish_title
   true
