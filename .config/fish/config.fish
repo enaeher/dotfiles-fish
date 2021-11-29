49cb445ff0541782fe985c5c5a1c9047256578fe
Eli Naeher <eli.naeher@shortcut.com>
2021-11-29 11:39:14 -0500

    mostly rename stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,15 @@
 set -x BROWSER chromium
 set -x EDITOR "emacsclient -t"
 set -x ALTERNATE_EDITOR "emacs --daemon"
-set -x CLUBHOUSE_HOME /Users/eli/clubhouse
-set -x AWS_PROFILE clubhouse
+set -x SHORTCUT_HOME /Users/elinaeher/shortcut
 set -x AWS_VAULT_BACKEND keychain
 set -x AWS_VAULT_KEYCHAIN_NAME login
 
 set -x JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
 set -x GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-ce-java11-19.3.1/Contents/Home
-set -x PATH $PATH $CLUBHOUSE_HOME/backend/bin $CLUBHOUSE_HOME/backend/infrastructure/build-support $GRAALVM_HOME/bin
+set -x PATH $SHORTCUT_HOME/backend/bin $SHORTCUT_HOME/backend/infrastructure/build-support $GRAALVM_HOME/bin $PATH
 
-source /usr/local/Cellar/autojump/22.5.3/share/autojump/autojump.fish
+source /usr/local/Cellar/autojump/22.5.3_3/share/autojump/autojump.fish
 set fish_greeting "Welcome."
 function fish_title
   true
@@ -18,3 +17,5 @@ end
 
 set -x GPG_TTY (tty)
 
+
+source /usr/local/opt/asdf/libexec/asdf.fish
