1d9a6149619672a606afd9dc899d2a9fe093e00d
Eli Naeher <eli@Elis-MacBook-Pro.local>
2024-12-02 12:39:16 -0500

    ...

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,12 @@
-set -x BROWSER chromium
+set -x BROWSER safari 
 set -x EDITOR "emacsclient -t"
 set -x ALTERNATE_EDITOR "emacs --daemon"
-set -x SHORTCUT_HOME /Users/elinaeher/shortcut
-set -x AWS_VAULT_BACKEND keychain
-set -x AWS_VAULT_KEYCHAIN_NAME login
 
-set -x JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
-set -x GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-ce-java11-19.3.1/Contents/Home
-set -x PATH $SHORTCUT_HOME/backend/bin $SHORTCUT_HOME/backend/infrastructure/build-support $GRAALVM_HOME/bin $PATH
+[ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
 
-source /usr/local/Cellar/autojump/22.5.3_3/share/autojump/autojump.fish
 set fish_greeting "Welcome."
 function fish_title
   true
 end
 
 set -x GPG_TTY (tty)
-
-
-source /usr/local/opt/asdf/libexec/asdf.fish
--- .config/fish/functions/docker.fish
+++
@@ -1,3 +0,0 @@
-function dkl -d "Authenticate with AWS ECR"
-  eval (aws --profile clubhouse ecr get-login --no-include-email --region us-east-1)
-end
